e6428ab2638b1326d5934034a50973446fdd1ba4
Mandinu Handapangoda <mandinu27@gmail.com>
2023-10-25 13:11:10 +0530

    shedule

--- src/main/java/com/example/sms/HelloApplication.java
+++ src/main/java/com/example/sms/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        stage.setTitle("CLUB HUB");
         stage.setScene(scene);
         stage.show();
     }
@@ -20,4 +20,5 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
 }
