52bd28cb7d9cf90ef364701946943e591d0d8e30
Yenuka <122987034+vgry5@users.noreply.github.com>
2023-10-27 11:14:31 +0530

    club class

--- src/main/java/com/example/sms/club.java
+++ src/main/java/com/example/sms/club.java
@@ -1,24 +1,22 @@
 package com.example.sms;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
 
-public class HelloController {
+public class club {
     private Stage stage;
     private Scene scene;
     private Parent root;
-    public void defolt (ActionEvent actionEvent) throws IOException {   // going to event view
 
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mainmenu.fxml")));
+    public void clubs(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("club.fxml")));
         stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         scene = new Scene(root, 744, 689);
         stage.setScene(scene);
