96bfe3a0966030979f25b2b3a4c54032bc2dad0e
Yenuka <122987034+vgry5@users.noreply.github.com>
2023-10-28 14:45:19 +0530

    styling

--- src/main/java/com/example/sms/menuController.java
+++ src/main/java/com/example/sms/menuController.java
@@ -1,18 +1,16 @@
 package com.example.sms;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
 
-public class HelloController {
+public class menuController {
     private Stage stage;
     private Scene scene;
     private Parent root;
