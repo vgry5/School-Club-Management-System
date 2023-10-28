d0d63ba562a53a37209c6b7dbd7a9f95bc9e932d
Yenuka <122987034+vgry5@users.noreply.github.com>
2023-10-28 14:27:53 +0530

    club class updated

--- src/main/java/com/example/sms/club.java
+++ src/main/java/com/example/sms/club.java
@@ -11,6 +11,9 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class club {
+    private String name;
+    private String description;
+    private int advisorID;
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -21,5 +24,31 @@ public class club {
         scene = new Scene(root, 744, 689);
         stage.setScene(scene);
         stage.show();
+    }
+    //Constructor
+    public club (String name,String description,int advisorID){
+        this.name = name;
+        this.description = description;
+        this.advisorID = advisorID;
+    }
+    //Getters and Setters(Encapsulation)
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public int getAdvisorID() {
+        return advisorID;
+    }
+    public void setAdvisorID(int advisorID) {
+        this.advisorID = advisorID;
     }
 }
