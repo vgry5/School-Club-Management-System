8d891b0cbd3731d2da28299c69b0b459e35b1c30
Yenuka <122987034+vgry5@users.noreply.github.com>
2023-10-30 10:52:14 +0530

    controllers

--- src/main/java/com/example/sms/clubController.java
+++ src/main/java/com/example/sms/clubController.java
@@ -13,44 +13,39 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
-public class mainmenucontroller {
+public class clubController {
+    @FXML
+    private Button clubbutton;
 
     @FXML
-    private Button adminbutton;
-
-    @FXML
-    private Button advisorbutton;
+    private Button existing;
 
     @FXML
     private AnchorPane container;
-
-    @FXML
-    private Button studentbutton;
 
     private Stage stage; //create variables for scene, stage and root
     private Scene scene;
     private Parent root;
 
-    public void studentpage(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("studentlogin.fxml")));
+    public void clubCreation(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("clubcreation.fxml")));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
-    public void staffpage(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("stafflogin.fxml")));
+    public void existingClubs(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("existingclubs.fxml")));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-
-    public void adminpage(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("adminlogin.fxml")));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+    public void clubs(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("club.fxml")));
+        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root, 744, 689);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/sms/mainmenuController.java
@@ -0,0 +1,57 @@
+package com.example.sms;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class mainmenuController {
+
+    @FXML
+    private Button adminbutton;
+
+    @FXML
+    private Button advisorbutton;
+
+    @FXML
+    private AnchorPane container;
+
+    @FXML
+    private Button studentbutton;
+
+    private Stage stage; //create variables for scene, stage and root
+    private Scene scene;
+    private Parent root;
+
+    public void studentpage(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("studentlogin.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void staffpage(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("stafflogin.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void adminpage(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("adminlogin.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
