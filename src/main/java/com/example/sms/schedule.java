4a5a381a7c10474f5bb6195603fb74d6a4525ef1
Mandinu Handapangoda <mandinu27@gmail.com>
2023-10-30 09:40:10 +0530

    new

--- src/main/java/com/example/sms/schedule.java
+++ src/main/java/com/example/sms/schedule.java
@@ -1,10 +1,13 @@
 package com.example.sms;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +17,21 @@ public class schedule {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    @FXML
+    private TextField EventName;
+    @FXML
+    private DatePicker date;
+    @FXML
+    private TextField location;
+
+
+    public void eventSchedule(ActionEvent event ) throws IOException{
+
+
+
+
+
+    }
     public void calender (ActionEvent actionEvent) throws IOException {   // going to calender view
 
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("calender.fxml")));
