f544bc1f445da261866880e89ad056f093877c0b
Mandinu Handapangoda <mandinu27@gmail.com>
2023-10-31 11:42:24 +0530

    database

---
+++ src/main/java/com/example/sms/DatabaseConnection.java
@@ -0,0 +1,31 @@
+package com.example.sms;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/clubmanager";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    private static Connection connection;
+
+    public static Connection connect() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (ClassNotFoundException e) {
+                throw new SQLException("MySQL JDBC driver not found.");
+            }
+        }
+        return connection;
+    }
+
+    public static void close() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.sms {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.sms to javafx.fxml;
     exports com.example.sms;
