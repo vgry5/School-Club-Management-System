6293f30589814fd91a1d879ea60d7a44f10715ce
lukmalilyas <lukmalilyas@gmail.com>
2023-11-14 09:49:42 +0530

    back button done

---
+++ src/main/java/com/example/sms/studentmenucontroller.java
@@ -0,0 +1,49 @@
+package com.example.sms;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class studentmenucontroller {
+
+    @FXML
+    private Button backbutton;
+
+    @FXML
+    private AnchorPane container;
+
+    @FXML
+    private Button joinbutton;
+
+    @FXML
+    private Button studentprofilebutton;
+
+    @FXML
+    private Button viewbutton;
+
+    @FXML
+    private Button withdrawbutton;
+
+    private Stage stage; //create variables for scene, stage and root
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    void back(ActionEvent event)throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("studentlogin.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
