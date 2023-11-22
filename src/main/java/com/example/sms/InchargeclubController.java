9608f9bd4966a2b038402a6363e71e4be0f7e074
Yenuka <122987034+vgry5@users.noreply.github.com>
2023-11-23 04:36:40 +0530

    comments

--- src/main/java/com/example/sms/InchargeclubController.java
+++ src/main/java/com/example/sms/InchargeclubController.java
@@ -34,19 +34,19 @@ public class InchargeclubController  implements Initializable {
         usernamecol.setCellValueFactory(new PropertyValueFactory<>("username"));
         namecol.setCellValueFactory(new PropertyValueFactory<>("firstname"));
         club AdvisorClub = null;
-        for(int x=0; x < OOPCoursework.clublist.size();x++){
+        for(int x=0; x < OOPCoursework.clublist.size();x++){//Finds the advisor's particular club
             if(OOPCoursework.clublist.get(x).getAdvisorID().equals(stafflogincontroller.username1)){
                 AdvisorClub = OOPCoursework.clublist.get(x);
                 break;
             }
         }
-        for(int z=0;z<OOPCoursework.studentList.size();z++){
+        for(int z=0;z<OOPCoursework.studentList.size();z++){//Search it with the student list and print the students in a particular club
             if(OOPCoursework.studentList.get(z).getClubs().contains(AdvisorClub)){
                 studtable.getItems().add(OOPCoursework.studentList.get(z));
             }
         }
     }
-    private club getAdvisorClub() {
+    private club getAdvisorClub() {//Use to retrieve the relevant club of the advisor
         club advisorClub = null;
         for (club club : OOPCoursework.clublist) {
             if (club.getAdvisorID().equals(stafflogincontroller.username1)) {
@@ -57,7 +57,7 @@ public class InchargeclubController  implements Initializable {
         return advisorClub;
     }
     @FXML
-    void back (ActionEvent event)throws IOException{
+    void back (ActionEvent event)throws IOException{//Goes back to the previous FXML
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("advisor.fxml")));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
@@ -66,24 +66,22 @@ public class InchargeclubController  implements Initializable {
     }
     @FXML
      void remove() throws SQLException {
-        Students  selectStudent = studtable.getSelectionModel().getSelectedItem();
-        studtable.getItems().remove(selectStudent);
-        System.out.println(selectStudent);
-        club advisorClub = getAdvisorClub();
+        Students  selectStudent = studtable.getSelectionModel().getSelectedItem();//Gets the  selected  club from the  table
+        studtable.getItems().remove(selectStudent);//Removes the above
+        club advisorClub = getAdvisorClub();//Stores the advisor club
         int i;
         for( i=0;i<OOPCoursework.studentList.size();i++){
             if(OOPCoursework.studentList.get(i).getUsername().equals(selectStudent.getUsername())){
-                ArrayList<club> studentClubs = selectStudent.getClubs();
-                studentClubs.remove(advisorClub);
+                ArrayList<club> studentClubs = selectStudent.getClubs();//Gets the list of clubs which  the particular student registered to
+                studentClubs.remove(advisorClub);//Removes the advisor club from the student's list of clubs
                 break;
             }
         }
-        System.out.println(advisorClub);
         String insertQuery =
                 "UPDATE students SET clubs = ? WHERE Username = ?";
         Connection connection = connectRegister.connect();
 
-        try(PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)){
+        try(PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)){//Updates the clubs column in student's table
             preparedStatement.setString(2,OOPCoursework.studentList.get(i).getUsername());
             preparedStatement.setString(1,OOPCoursework.studentList.get(i).clubString());
             int affectedRow = preparedStatement.executeUpdate();
