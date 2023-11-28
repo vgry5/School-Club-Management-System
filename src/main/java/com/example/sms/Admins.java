c611cdb5586b19aa156b0b849c9afeb333486542
Yenuka <gvryeth@gmail.com>
2023-11-28 23:36:08 +0530

    admin done

--- src/main/java/com/example/sms/Admins.java
+++ src/main/java/com/example/sms/Admins.java
@@ -1,47 +1,8 @@
 package com.example.sms;
 
-public class Admins {
-    String Firstname;
-    String Lastname;
-    String Username;
-    String Password;
-
+public class Admins extends Person{
     public Admins(String firstname, String lastname, String username, String password) {
-        Firstname = firstname;
-        Lastname = lastname;
-        Username = username;
-        Password = password;
-    }
-
-    public String getFirstname() {
-        return Firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        Firstname = firstname;
-    }
-
-    public String getLastname() {
-        return Lastname;
-    }
-
-    public void setLastname(String lastname) {
-        Lastname = lastname;
-    }
-
-    public String getUsername() {
-        return Username;
-    }
-
-    public void setUsername(String username) {
-        Username = username;
-    }
-
-    public String getPassword() {
-        return Password;
-    }
-
-    public void setPassword(String password) {
-        Password = password;
+        super(firstname,lastname,username,password);
     }
 }
+
--- src/main/java/com/example/sms/mainmenuController.java
+++ src/main/java/com/example/sms/mainmenuController.java
@@ -9,41 +9,28 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.sql.Connection;
 import java.util.Objects;
-
 public class mainmenuController {
-
     @FXML
     private Button adminbutton;
-
     @FXML
     private Button advisorbutton;
-
     @FXML
     private AnchorPane container;
-
     @FXML
     private Button studentbutton;
-
     private Stage stage; //create variables for scene, stage and root
     private Scene scene;
     private Parent root;
-
-
-
     public void studentpage(ActionEvent event) throws IOException {
         root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("studentlogin.fxml")));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-
-      //  connect = DatabaseConnection.connect();   sql  connection
+        //  connect = DatabaseConnection.connect();   sql  connection
     }
-
     public void staffpage(ActionEvent event) throws IOException {
         root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("stafflogin.fxml")));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -51,7 +38,6 @@ public class mainmenuController {
         stage.setScene(scene);
         stage.show();
     }
-
     public void adminpage(ActionEvent event) throws IOException {
         root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("adminlogin.fxml")));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
